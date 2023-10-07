6e877cb9cbfff9269fce97fa876b1562079bd08a
Ann-Ryazanova <zai97@mail.ru>
2023-10-07 19:07:36 +0300

    2

--- mailing/models.py
+++ mailing/models.py
@@ -1,30 +1,38 @@
-from django.utils import timezone
+from datetime import datetime
 
 from django.db import models
 
 from client.models import Client, NULLABLE
 from django.db.models import TextChoices
-
-
-class Status(TextChoices):
-    ACTIVE = 'AC', 'Active'
-    FINISHED = 'FI', 'Finished'
-    CREATED = 'CR', 'Created'
-
-
-class Period(TextChoices):
-    PERIOD_DAILY = 'DA', 'daily'
-    PERIOD_WEEKLY = 'WE', 'weekly'
-    PERIOD_MONTHLY = 'MO', 'monthly'
+from django.utils import timezone
 
 
 class MailingSettings(models.Model):
+    PERIOD_DAILY = 'daily'
+    PERIOD_WEEKLY = 'weekly'
+    PERIOD_MONTHLY = 'monthly'
 
-    period = models.CharField(max_length=20, choices=Period.choices, verbose_name='периодичность рассылки',
-                              default=Period.PERIOD_DAILY)
-    status = models.CharField(max_length=20, choices=Status.choices, verbose_name='статус рассылки', default=Status.CREATED)
-    time = models.TimeField(verbose_name="Время отправки")
+    PERIODS = (
+        (PERIOD_DAILY, 'Ежедневная'),
+        (PERIOD_WEEKLY, 'Раз в неделю'),
+        (PERIOD_MONTHLY, 'Раз в месяц'),
+    )
 
+    STATUS_CREATED = 'created'
+    STATUS_STARTED = 'started'
+    STATUS_DONE = 'done'
+
+    STATUSES = (
+        (STATUS_STARTED, 'Запущена'),
+        (STATUS_CREATED, 'Создана'),
+        (STATUS_DONE, 'Завершена'),
+    )
+
+    period = models.CharField(max_length=20, choices=PERIODS, verbose_name='периодичность рассылки',
+                              default=PERIOD_DAILY)
+    status = models.CharField(max_length=20, choices=STATUSES, verbose_name='статус рассылки', default=STATUS_CREATED)
+    time = models.TimeField(default=datetime.now, verbose_name='время рассылки')
+    day = models.DateField(default=datetime.today, verbose_name='Дата начала')
     topic_mail = models.CharField(max_length=100, verbose_name='Тема письма')
     body_mail = models.CharField(max_length=100, verbose_name='Тело письма')
 
@@ -47,12 +55,11 @@ class MailingSettings(models.Model):
 
 class Logs(models.Model):
     last_attempt_send = models.DateTimeField(default=None, verbose_name="дата и время последней попытки")
-    status_send = models.BooleanField(default=False, verbose_name='Статус попытки')
-
+    status_send = models.CharField(max_length=150, verbose_name='Статус попытки')
     # Не понимаю как задать пользователя, которому отправлялось письмо
     mailing = models.ForeignKey('MailingSettings', on_delete=models.CASCADE,
                                 verbose_name='Рассылка,которая отправлялась')
-    #client = models.ForeignKey('Client', on_delete=models.CASCADE, verbose_name='Получатель рассылки')
+    # client = models.ForeignKey('Client', on_delete=models.CASCADE, verbose_name='Получатель рассылки')
 
     last_attempt_response = models.CharField(max_length=150, **NULLABLE, verbose_name='ответ почтового сервера')
 
