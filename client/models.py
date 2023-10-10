8af1f6c8523bbacdd58f51536e887a9f137473b9
Ann-Ryazanova <zai97@mail.ru>
2023-10-10 22:26:00 +0300

    cw_django

--- client/models.py
+++ client/models.py
@@ -1,24 +1,21 @@
 from django.db import models
+from users.models import User
 
 NULLABLE = {'blank': True, 'null': True}
 
 
 class Client(models.Model):
-    # Тот, кто получает письмо должен быть
-    email = models.EmailField(unique=True, verbose_name='почта')
+    email = models.EmailField(verbose_name='почта')
 
     first_name = models.CharField(max_length=100, verbose_name='Имя', **NULLABLE)
     last_name = models.CharField(max_length=100, verbose_name='Фамилия', **NULLABLE)
     middle_name = models.CharField(max_length=100, verbose_name='Отчество', **NULLABLE)
     comment = models.TextField(verbose_name='комментарий', **NULLABLE)
 
-    #Должно ли быть поле владелец клиента
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
 
     def __str__(self):
         return self.email
-                #f'{self.first_name}, {self.last_name}'
-                #f'{self.middle_name}, {self.comment}')
 
     class Meta:
         verbose_name = 'клиент'
