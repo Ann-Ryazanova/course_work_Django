e158bc7198225b2646d047c213082de219433c50
Ann-Ryazanova <zai97@mail.ru>
2023-10-10 22:25:45 +0300

    cw_django

--- client/forms.py
+++ client/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 
 from client.models import Client
-from mailing.forms import StyleFormMixin
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class ClientForm(StyleFormMixin, forms.ModelForm):
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # привязываем текущего пользователя к полю user
+        return super().form_valid(form)
 
     class Meta:
         model = Client
