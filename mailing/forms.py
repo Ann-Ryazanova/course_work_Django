b7995fc0a6696bb86afd0955c490a53b540fab00
Ann-Ryazanova <zai97@mail.ru>
2023-10-10 22:25:48 +0300

    cw_django

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,18 +1,22 @@
 from django import forms
-from django.conf import settings
-from django.forms import DateField, DateTimeField
 
+from client.models import Client
 from mailing.models import MailingSettings
 
 
-class StyleFormMixin:
+class MailingSettingsForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        user = kwargs.pop('user', None)
+        super(MailingSettingsForm, self).__init__(*args, **kwargs)
+
+        # Ограничиваем queryset для выбора клиентов только теми, которых создал текущий пользователь
+        self.fields['clients'].queryset = Client.objects.filter(user=user)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
-
-class MailingSettingsForm(StyleFormMixin, forms.ModelForm):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
     class Meta:
         model = MailingSettings
