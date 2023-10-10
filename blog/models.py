0a3aa119e0eba3213c448490db7a43fb0afdfd03
Ann-Ryazanova <zai97@mail.ru>
2023-10-10 22:28:34 +0300

    cw_django

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    content = models.TextField(verbose_name='содержимое')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    date_of_creation = models.DateField(verbose_name='Дата создания', auto_now_add=True, **NULLABLE)
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
