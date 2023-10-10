05145eaef218ac6aa263a41ab52ae6e8ed3d9e51
Ann-Ryazanova <zai97@mail.ru>
2023-10-10 22:26:25 +0300

    cw_django

--- mailing/urls.py
+++ mailing/urls.py
@@ -8,7 +8,6 @@ app_name = MailingConfig.name
 urlpatterns = [
     path('create/', MailingSettingsCreateView.as_view(), name='create_mailing'),
     path('', MailingSettingsListView.as_view(), name='list'),
-    #path('contact/', MailingContactView.as_view(), name='contacts'),
     path('product/<int:pk>/', MailingSettingsDetailView.as_view(), name='mailing_detail'),
     path('edit/<int:pk>/', MailingSettingsUpdateView.as_view(), name='update_mailing'),
     path('delete/<int:pk>/', MailingSettingsDeleteView.as_view(), name='delete_mailing'),
